ffc59446ff4317c16734aa5b20324f71e4a1bd8c
vermad843 <vishnuverma7524@gmail.com>
2019-10-06 03:00:14 +0530

    recent screams at the top and what time and date they are posted

---
+++ functions/dbschema.js
@@ -0,0 +1,13 @@
+//how our data should look like .
+
+let db = {
+    screams : [
+        {
+            userHandle : 'user',
+            body : 'this is the screen body',
+            createdAt : '2019-10-05T21:18:29.208Z',
+            likeCount : 5 ,
+            commentCount : 2
+        }
+    ]
+}
--- functions/index.js
+++ functions/index.js
@@ -7,39 +7,49 @@ const express = require('express');
 const app = express();
 
 
-app.get('/screams', (req,res) => {
-  admin
-    .firestore()
-    .collection('screams')
-    .get()
-    .then((data) => {
-        let screams = [];
-        data.forEach((doc) => {
-            screams.push(doc.data());
-        });
-        return res.json(screams);
-    })
-    .catch((err) => console.error(err));   
+app.get('/screams', (req, res) => {
+    admin
+        .firestore()
+        .collection('screams')
+        .orderBy('createdAt', 'desc')
+        .get()
+        .then((data) => {
+            let screams = [];
+            data.forEach((doc) => {
+                screams.push({
+                    screamId: doc.id,
+                    body: doc.data().body,
+                    userHandle: doc.data().userHandle,
+                    createdAt: doc.data().createdAt
+                });
+            });
+            return res.json(screams);
+        })
+        .catch((err) => console.error(err));
 });
 
 
 
-app.post('/scream', (req,res) => {
+app.post('/scream', (req, res) => {
     const newScream = {
         body: req.body.body,
-        userHandle : req.body.userHandle,
-        createdAt : admin.firestore.Timestamp.fromDate(new Date())
+        userHandle: req.body.userHandle,
+        createdAt: new Date().toISOString()
     };
     admin.firestore()
-     .collection('screams')
-     .add(newScream)
-     .then(doc => {
-         res.json({message : `document ${doc.id} created successfully`});
-     })
-     .catch(err => {
-         res.status(500).json({error : 'something went wrong'});
-         console.error(err)
-     })
+        .collection('screams')
+        .add(newScream)
+        .then(doc => {
+            res.json({
+                message: `document ${doc.id} created successfully`
+            });
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: 'something went wrong'
+            });
+            console.error(err)
+        })
 });
 
 
