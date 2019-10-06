8567db3caa3ae2977e1c66536fa46515e2957f5d
vermad843 <vishnuverma7524@gmail.com>
2019-10-07 03:44:29 +0530

    handling email,password and handle errors

--- functions/index.js
+++ functions/index.js
@@ -3,6 +3,7 @@ const admin = require('firebase-admin');
 const express = require('express');
 const app = express();
 
+
 admin.initializeApp();
 
 const config = {
@@ -22,6 +23,7 @@ const firebase = require('firebase');
 firebase.initializeApp(config);
 
 const db = admin.firestore();
+
 
 
 app.get('/screams', (req, res) => {
@@ -46,7 +48,7 @@ app.get('/screams', (req, res) => {
 
 
 
-app.post('/scream', (req, res) => {
+app.post('/scream', (req, res,next) => {
     const newScream = {
         body: req.body.body,
         userHandle: req.body.userHandle,
@@ -68,6 +70,18 @@ app.post('/scream', (req, res) => {
         })
 });
 
+const isEmail = (email) => {
+    const regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    if(email.match(regEx)) return true;
+    else return false;
+}
+
+
+const isEmpty = (string) => {
+    if(string.trim() === '') return true;
+    else return false ;
+}
+
 
 app.post('/signup', (req,res) => {
     const newUser = {
@@ -77,7 +91,21 @@ app.post('/signup', (req,res) => {
         handle : req.body.handle,
     };
 
- // validating a user 
+    let errors = {};
+
+    if(isEmpty(newUser.email)) {
+        errors.email = 'Must not be empty'
+    }else if(!isEmail(newUser.email)) {
+        errors.email = 'Must be a valid email address'
+    }
+
+    if(isEmpty(newUser.password)) errors.password = 'Must not be empty'
+    if(newUser.password !== newUser.confirmPassword) errors.confirmPassword = 'Passwords must match';
+    if(isEmpty(newUser.handle)) errors.handle = 'Must not be empty';
+
+    if(Object.keys(errors).length > 0) return res.status(400).json(errors);
+
+ 
 
  db.doc(`/users/${newUser.handle}`)
    .get()
@@ -99,10 +127,9 @@ app.post('/signup', (req,res) => {
    .catch(err => {
        console.error(err);
        return res.status(500).json({error : err.code});
-   })
+   });
 
-
-});
+  });
 
 
 exports.api = functions.https.onRequest(app);
