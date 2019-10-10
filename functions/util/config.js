0c2db8c5ac496cb23654e45f940247ef0cbac717
vermad843 <vishnuverma7524@gmail.com>
2019-10-10 05:53:50 +0530

    restructring our code(there is a login error)

---
+++ functions/util/config.js
@@ -0,0 +1,11 @@
+
+module.exports = {
+    apiKey: "AIzaSyDTDcGTbkb1p22FzTrjk2u_7O6YLjLpx5s",
+    authDomain: "socialape-f4797.firebaseapp.com",
+    databaseURL: "https://socialape-f4797.firebaseio.com",
+    projectId: "socialape-f4797",
+    storageBucket: "socialape-f4797.appspot.com",
+    messagingSenderId: "248864740682",
+    appId: "1:248864740682:web:4ad5f52a7e8bf6f2f85db3",
+    measurementId: "G-JSWKZ2Q4N6"
+  };
