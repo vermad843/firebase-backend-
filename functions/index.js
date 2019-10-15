9e228ee024a036e4a34d0c8d88cd4dd1c993f3fb
vermad843 <vishnuverma7524@gmail.com>
2019-10-15 09:17:52 +0530

    adding comments array in our database

--- functions/index.js
+++ functions/index.js
@@ -4,20 +4,21 @@ const app = express();
 
 const FBAuth = require('./util/fbAuth');
 
-const { getAllScreams, postOneScream} = require('./handlers/screams');
+const { getAllScreams, postOneScream,getScream} = require('./handlers/screams');
 const { signup, login, uploadImage, addUserDetails,getAuthenticatedUser } = require('./handlers/users');
 
 //screams routes 
 app.get('/screams',getAllScreams );
 app.post('/scream',FBAuth, postOneScream);
-app.post('/user/image' ,FBAuth, uploadImage );
-app.post('/user' ,FBAuth, addUserDetails );
-app.get('/user' ,FBAuth, getAuthenticatedUser);
+app.get('/scream/:screamId',getScream);
 
 
 //users routes 
 app.post('/signup', signup);
 app.post('/login' , login );
+app.post('/user/image' ,FBAuth, uploadImage );
+app.post('/user' ,FBAuth, addUserDetails );
+app.get('/user' ,FBAuth, getAuthenticatedUser);
 
 
 
